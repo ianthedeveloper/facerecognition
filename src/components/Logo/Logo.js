5e5359a7d7b819838998059a7e85521fe7d77206
mungaii <theforexer1@gmail.com>
2024-04-25 21:11:22 +0300

    Adding Components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './logo.css';
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+    return (
+        <div>
+            <Tilt>
+                <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
+                    <h1>React Parallax Tilt 👀</h1>
+                </div>
+            </Tilt>
+
+        </div>
+    )
+}
+
+export default Logo;
