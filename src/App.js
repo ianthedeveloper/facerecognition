312ddce10aab7b7d05c2f079cf166558381903c1
mungaii <theforexer1@gmail.com>
2024-05-01 10:20:55 +0300

    Modifications: Adjustments, Clarifai fetch() code, App Component

--- src/App.js
+++ src/App.js
@@ -10,13 +10,12 @@ import { jsx } from 'react/jsx-runtime';
 
 
 const returnClarifaiRequestOptions = (imageUrl) => {
-  const PAT = '70d2b99eec204856bc2886b35b22b05d';
-  // const PAT = '86a7c1fb3a564cef9792728daeaae130';
+  // const PAT = '70d2b99eec204856bc2886b35b22b05d';
+  const PAT = '86a7c1fb3a564cef9792728daeaae130';
   const USER_ID = 'ianthedeveloper';
   const APP_ID = 'my-first-application';
-  const MODEL_ID = 'face-detection';
   const IMAGE_URL = imageUrl;
-
+  
   const raw = JSON.stringify({
     "user_app_id": {
         "user_id": USER_ID,
@@ -60,25 +59,20 @@ class App extends Component {
     console.log(event.target.value);
     this.setState({input: event.target.value});
   }
-
+  
   onButtonSubmit = (event) => {
+    const MODEL_ID = 'face-detection';
+
     console.log("Click");
     this.setState({imageUrl: this.state.input})
 
     fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(this.state.input))
-        .then(response => {
-          response.json()
-          console.log("Response", response)
-        })
-        .then(result => console.log("Result", result))
-        .catch(error => console.log('Ooops! There was an error', error));
-
-    // fetch("https://api.clarifai.com/v2/models/" + 'MODEL-ID' + "/outputs", returnClarifaiRequestOptions(this.state.input))
-    // .then(response => {
-    //   response.json()
-    //   console.log(response);
-    // })
+    .then(response => {
+      response.json()
+      console.log(response);
+    })
     // .then(result => {
+    //     // console.log("Result 1", result)
     //     const regions = result.outputs[0].data.regions;
 
     //     regions.forEach(region => {
@@ -96,11 +90,37 @@ class App extends Component {
 
     //             console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
                 
+    //             // console.log("Result 2", result);
     //         });
     //     });
-    //     console.log(result);
     // })
     // .catch(error => console.log('Ooops! There was an error', error));
+
+    .then(result => {
+      console.log(result); // Log the entire result object to see its structure
+      if (result && result.outputs && result.outputs.length > 0) {
+          const regions = result.outputs[0].data.regions;
+          regions.forEach(region => {
+              // Accessing and rounding the bounding box values
+              const boundingBox = region.region_info.bounding_box;
+              const topRow = boundingBox.top_row.toFixed(3);
+              const leftCol = boundingBox.left_col.toFixed(3);
+              const bottomRow = boundingBox.bottom_row.toFixed(3);
+              const rightCol = boundingBox.right_col.toFixed(3);
+  
+              region.data.concepts.forEach(concept => {
+                  // Accessing and rounding the concept value
+                  const name = concept.name;
+                  const value = concept.value.toFixed(4);
+  
+                  console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+              });
+          });
+      } else {
+          console.log('Invalid response structure'); // Handle invalid response
+      }
+    })
+    .catch(error => console.log('Ooops! There was an error', error));
   }
 
 
