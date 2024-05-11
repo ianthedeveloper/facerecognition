9e43902ef82c81e0a3b1e5faa21a3ecbf2ef5bab
ianthedeveloper <thedeveloperian@gmail.com>
2024-05-11 20:59:41 +0300

    Modifications: Changed Rank prompt, Rank Component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = () => {
     return (
         <div className="mb0">
-            <p className="f3">{"Mungaii, your rank is:"}</p>
+            <p className="f3">{"Dear user, you have a rank of :"}</p>
             <p className="f3">{"#3"}</p>
         </div>
     )
