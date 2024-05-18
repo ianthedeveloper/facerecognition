d9054183b0e9abd176e5711de9c62ea3d7a2e964
ianthedeveloper <thedeveloperian@gmail.com>
2024-05-18 08:25:23 +0300

    Used ChatGPT to correct syntaxial error, App.js

--- src/App.js
+++ src/App.js
@@ -125,6 +125,114 @@
 
 // CODE FROM Discord
 
+// import React, { useState } from 'react';
+// import './App.css';
+// import ParticlesBg from 'particles-bg';
+// import Clarifai from 'clarifai';
+// import Navigation from './components/Navigation/Navigation';
+// import Logo from './components/Logo/Logo';
+// import Rank from './components/Rank/Rank';
+// import ImageProcessingField from './components/ImageProcessingField/ImageProcessingField';
+// import Facerecognition from './components/Facerecognition/Facerecognition';
+
+
+
+// const app = new Clarifai.App({
+//   apiKey: 'b99b35c39d5a491d956f588c06afd1a0'
+// });
+  
+  
+// const returnClarifaiRequestOptions = (imageUrl) => {
+  
+//   const PAT = '70d2b99eec204856bc2886b35b22b05d'; 
+//   const USER_ID = 'ianthedeveloper';       
+//   const APP_ID = 'my-first-application';
+//   const IMAGE_URL = imageUrl;
+  
+//   const raw = JSON.stringify({
+//       "user_app_id": {
+//           "user_id": USER_ID,
+//           "app_id": APP_ID
+//       },
+//       "inputs": [
+//           {
+//               "data": {
+//                   "image": {
+//                       "url": IMAGE_URL
+//                   }
+//               }
+//           }
+//       ]
+//   });
+
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Authorization': 'Key' + PAT
+//     },
+//     body: raw
+//   };
+    
+//   return requestOptions;
+
+// }
+  
+// function App () {
+//   const MODEL_ID = 'face-detection';   
+//   const [input, setInput] = useState('');
+//   const [imageUrl, setImageUrl] = useState('');
+  
+//   const onInputChange = (event) => {
+//     console.log(event.target.value);
+//     setInput(input : event.target.value);
+//   }
+//   const onButtonSubmit = () => {
+//     setImageUrl(input);
+//     app.models.predict('face-detection',input)
+//     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", 
+//     returnClarifaiRequestOptions(input))
+//     .then(response => response.json())
+//     .then(result => console.log(result))
+//     .then(
+//       function(response){
+//         console.log(response);
+//       },
+//       function(err){
+      
+//       }
+//     );
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <ParticlesBg className="particlesBg" num={331} type="fountain" bg={true} />
+//         <Navigation/>
+//         <Logo/>
+//         <Rank/>
+//         <ImageProcessingField 
+//           onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}  
+//         />
+//         <Facerecognition imageUrl={imageUrl}  />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+// CORRECTED DISCORD CODE BY ChatGPT
+
 import React, { useState } from 'react';
 import './App.css';
 import ParticlesBg from 'particles-bg';
@@ -135,89 +243,84 @@ import Rank from './components/Rank/Rank';
 import ImageProcessingField from './components/ImageProcessingField/ImageProcessingField';
 import Facerecognition from './components/Facerecognition/Facerecognition';
 
-
-
 const app = new Clarifai.App({
   apiKey: 'b99b35c39d5a491d956f588c06afd1a0'
 });
-  
-  
+
 const returnClarifaiRequestOptions = (imageUrl) => {
-  
-  const PAT = '70d2b99eec204856bc2886b35b22b05d'; 
-  const USER_ID = 'ianthedeveloper';       
+  const PAT = '70d2b99eec204856bc2886b35b22b05d';
+  const USER_ID = 'ianthedeveloper';
   const APP_ID = 'my-first-application';
   const IMAGE_URL = imageUrl;
-  
+
   const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": IMAGE_URL
-                  }
-              }
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
           }
-      ]
+        }
+      }
+    ]
   });
 
   const requestOptions = {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key' + PAT
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT // Added space after 'Key'
     },
     body: raw
   };
-    
-  return requestOptions;
 
+  return requestOptions;
 }
-  
-function App () {
-  const MODEL_ID = 'face-detection';   
+
+function App() {
+  const MODEL_ID = 'face-detection';
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  
+
   const onInputChange = (event) => {
     console.log(event.target.value);
-    setInput({input : event.target.value});
+    setInput(event.target.value); // Removed wrapping input with curly braces
   }
+
   const onButtonSubmit = () => {
     setImageUrl(input);
-    app.models.predict('face-detection',input)
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", 
-    returnClarifaiRequestOptions(input))
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .then(
-      function(response){
+    app.models.predict('face-detection', input)
+      .then(response => {
         console.log(response);
-      },
-      function(err){
-      
-      }
-    );
+        return fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(input))
+      })
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
   }
-  render() {
-    return (
-      <div className="App">
-        <ParticlesBg className="particlesBg" num={331} type="fountain" bg={true} />
-        <Navigation/>
-        <Logo/>
-        <Rank/>
-        <ImageProcessingField 
-          onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  
-        />
-        <Facerecognition imageUrl={this.state.imageUrl}  />
-      </div>
-    );
-  }
+
+  return (
+    <div className="App">
+      <ParticlesBg className="particlesBg" num={331} type="fountain" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageProcessingField
+        onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}
+      />
+      <Facerecognition imageUrl={imageUrl} />
+    </div>
+  );
 }
 
 export default App;
+
+
+
+
+
 
