b3676c7d553055142842fd81423ee0a0719af034
ianthedevveloper <thedeveloperian@gmail.com>
2024-05-21 09:48:30 +0300

    Deleted ChatGPT code, Appmjs

--- src/App.js
+++ src/App.js
@@ -117,36 +117,6 @@ export default App;
 
 
 
-// ADDITIONAL CODE FROM ChatGPT
 
-    // fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", returnClarifaiRequestOptions(this.state.input))
-    // .then(response => {
-    //   response.json()
-    //   console.log("Response", response);
-    // })
-    // .then(result => {
-    //   console.log("Result", result); // Log the entire result object to see its structure
-    //   if (result && result.outputs && result.outputs.length > 0) {
-    //       const regions = result.outputs[0].data.regions;
-    //       regions.forEach(region => {
-    //           // Accessing and rounding the bounding box values
-    //           const boundingBox = region.region_info.bounding_box;
-    //           const topRow = boundingBox.top_row.toFixed(3);
-    //           const leftCol = boundingBox.left_col.toFixed(3);
-    //           const bottomRow = boundingBox.bottom_row.toFixed(3);
-    //           const rightCol = boundingBox.right_col.toFixed(3);
-
-    //           region.data.concepts.forEach(concept => {
-    //               // Accessing and rounding the concept value
-    //               const name = concept.name;
-    //               const value = concept.value.toFixed(4);
-
-    //               console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-    //           });
-    //       });
-    //   } else {
-    //       console.log('Invalid response structure'); // Handle invalid response
-    //   }
-    // })
 
 
